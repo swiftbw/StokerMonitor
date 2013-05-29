6a98af6028d0843fa74cfbe3a918ec5c9654a0f1
Brian Swift <swiftb@Brians-MacBook-Air.local>
2013-05-28 19:36:37 -0500

    Cleaned up httpget function

--- httpget.py
+++ httpget.py
@@ -1,32 +1,40 @@
 import httplib
-import StokerHTMLParser
 from datetime import datetime
 import time
 
 
-conn = httplib.HTTPConnection("192.168.1.50")
-
-while 1:
-    conn.request("GET", "/index.html")
-    r1 = conn.getresponse()
-
-    data = r1.read()
-    print r1.status, r1.reason
-    print data
-
-    now = datetime.now()
+class httpgetter:
     
-    stokerOutFile = "Stoker" + now.strftime('%Y%m%d%H%M%S')
+    def __init__(self, ipaddress, urlpage):
+        self.ipaddress = ipaddress
+        self.urlpage = urlpage
+        self.conn = httplib.HTTPConnection(self.ipaddress)
+
+    def getResponse(self):
+        self.conn.request("GET", self.urlpage)
+        response = self.conn.getresponse()
+
+        return (response)
     
-    f = open(stokerOutFile, 'w')
+def main():
+    stokehttp = httpgetter('192.168.1.50', '/index.html')
 
-    f.write(data)
+    while 1:
+        resp = stokehttp.getResponse()
+        
+        data = resp.read()
 
-    f.close()
+        now = datetime.now()
     
-        #par = StokerHTMLParser.StokerHTMLParser()
-    time.sleep(60)
+        stokerOutFile = "Stoker" + now.strftime('%Y%m%d%H%M%S') + '.skr'
     
+        f = open(stokerOutFile, 'w')
 
-par.feed(data)
+        f.write(data)
 
+        f.close()
+    
+        time.sleep(60)
+    
+if __name__ == '__main__':
+    main()
